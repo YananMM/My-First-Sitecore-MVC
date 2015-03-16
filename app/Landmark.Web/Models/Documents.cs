dbe324b144c6ad5b7a9d195a61225b5ca571551d
YananMM <yananfun@outlook.com>
2015-03-16 20:06:28 +0800

    first init landmark test project

---
+++ app/Landmark.Web/Models/Documents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Data.Items;
+using Glass.Mapper;
+using Glass.Mapper.Sc;
+using Landmark.Web.Models;
+using Landmark.Web.Models.sitecore.templates.User_Defined.MyFirstSitecoreMVC;
+
+namespace Landmark.Web.Models
+{
+    public class Documents:List<Document_Item>, IRenderingModel
+    {
+        public void Initialize(Rendering rendering)
+        {
+            foreach (Item child in rendering.Item.Children)
+            {
+                this.Add(child.GlassCast<Document_Item>());
+            }
+        }
+    }
+}
