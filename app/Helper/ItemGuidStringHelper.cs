0ec23d57ee9128c83cc68b8ca825489c45d3f22f
YananMM <yananfun@outlook.com>
2015-03-31 16:09:33 +0800

    update rendering view structure and add GlossaryRendering.cshtml

---
+++ app/Helper/ItemGuidStringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Landmark.Helper
+{
+    public abstract class ItemGuidStringHelper
+    {
+        //rendering guids
+        public static readonly String GlossaryRenderingViewGuid = "{8DB7585D-4A38-4687-A72F-B33AF97B6369}";
+        public static readonly String ScriptRenderingViewGuid = "{6DB62DA9-0028-4812-BF51-5CC899DC8B66}";
+        public static readonly String FooterRenderingViewGuid = "{C34F6069-D5B7-4D4A-97A4-DFEB53AE6F1C}";
+
+
+        //item guids
+        public static readonly string ScrollToBeginItemGuid = "{A56115E2-9083-4C3C-AC15-3AB1FEB4A90B}";
+        public static readonly string TopItemGuid = "{95BFC753-B808-40FD-9380-78CED4E3D341}";
+
+    }
+}
