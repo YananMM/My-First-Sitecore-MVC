302669d5342613d346857fed8b554d574e48114d
LilyJiang <lily.jiang@gruden.com>
2015-04-01 17:19:21 +0800

    modify footer
    Signed-off-by: LilyJiang <lily.jiang@gruden.com>

--- app/Classes/ItemGuids.cs
+++ app/Classes/ItemGuids.cs
@@ -13,8 +13,10 @@ namespace Landmark.Classes
         public static readonly String ScriptRenderingViewGuid = "{6DB62DA9-0028-4812-BF51-5CC899DC8B66}";
         public static readonly String FooterRenderingViewGuid = "{C34F6069-D5B7-4D4A-97A4-DFEB53AE6F1C}";
         public static readonly String PanelNavRenderingViewGuid = "{BE3E7C0F-02E0-4E61-A62D-13E475CF76B5}";
-        public static readonly String HeaderRenderingViewGuid = "{74159145-AA53-4198-9506-0B451AB91E02}"; 
-       
+        public static readonly String HeaderRenderingViewGuid = "{74159145-AA53-4198-9506-0B451AB91E02}";
+        public static readonly String FooterNavRenderingViewGuid = "{3BCEEFB7-E383-41B5-B42D-CF08898CF378}";
+        public static readonly String FooterConfigRenderingViewGuid = "{59CD0D0B-B8B3-4849-8A2E-2AD9A0D1CA46}";
+
 
 
         //item id guids
@@ -23,13 +25,14 @@ namespace Landmark.Classes
         public static readonly String PanelsFolderItemGuid = "{3BB40D54-8F7F-449B-B9DB-432C65EB4224}";
         public static readonly String TraditionalChineseItemGuid = "{B9A164BF-C9D5-486E-96A5-229F960D6FFD}";
         public static readonly String SimplifiedChineseItemGuid = "{C6222FDA-DA27-41EF-A5E4-D5201FF82C64}";
-        
+
 
 
         //item 
         public static readonly Item LandmarkHomeItem = Sitecore.Context.Database.GetItem("{691A2D60-6CAF-42B7-90ED-7DD1AE99AFF3}");
         public static readonly Item SearchPlaceHolderItem = Sitecore.Context.Database.GetItem("{69752992-6EA7-4FE6-8925-E279B4469B9F}");
         public static readonly Item LandmarkConfigItem = Sitecore.Context.Database.GetItem("{BDB18DFC-7C97-40F8-A80C-658210B41656}");
+        public static readonly Item FooterNavigationItem = Sitecore.Context.Database.GetItem("{9B0D8F54-71B1-41A5-B1F9-B1799DB44C6A}");
 
     }
 }
