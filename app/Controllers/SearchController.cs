18cfaa8cd9b99f8411a8bf7c13b2b91d8b25c044
YananMM <yananfun@outlook.com>
2015-04-08 15:03:17 +0800

    update content search

--- app/Controllers/SearchController.cs
+++ app/Controllers/SearchController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Landmark.Models;
 using Sitecore.ContentSearch;
 using Sitecore.ContentSearch.SearchTypes;
 using Sitecore.Shell.Framework.Commands.UserManager;
 using Sitecore.Configuration;
 using Sitecore.ContentSearch.Linq;
+using Sitecore.ContentSearch.Linq.Utilities;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Shell.Web.UI.WebControls;
 
 namespace Landmark.Controllers
 {
@@ -26,15 +30,31 @@ namespace Landmark.Controllers
                 var language = Sitecore.Context.Language.Name.ToLower();
                 string indexName = Settings.GetSetting("LandmarkIndexName");
                 var index = ContentSearchManager.GetIndex(indexName);
+                var query = PredicateBuilder.True<LandmarkSearchResultItem>();
+
+
+                query = query.And(x => x.PageTitle.Contains(search));
+
+
                 using (var context = index.CreateSearchContext())
                 {
+
                     var searchItems = context.GetQueryable<SearchResultItem>()
-                        .Where(item => item.Content.Like(search) && item.Language.Equals(language)
-                        ).ToList();
-                    return Content(searchItems[0].ItemId.ToString());
+                        .Where(item => item.Content.Like(search) && item.Language.Equals(language))
+                        .OrderBy(item => item.CreatedDate)
+                        .ToList();
+                    var total = searchItems.Count;
+                    if (total > 0)
+                    {
+                        return Content(total.ToString());
+                    }
+                    else
+                    {
+                        return Content("No Search Result");
+                    }
                 }
             }
-            return Content("");
+            
         }
     }
 }
---
+++ app/Models/LandmarkSearchResultItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Links;
+
+namespace Landmark.Models
+{
+    
+    public class LandmarkSearchResultItem : SearchResultItem
+    {
+        [IndexField("Page Title")]
+        public string PageTitle { get; set; }
+
+        
+    }
+}
