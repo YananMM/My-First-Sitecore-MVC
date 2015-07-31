1120748b2b6ba4d65b7114102019d0a2ff042461
LilyJiang <lily.jiang@gruden.com>
2015-07-31 17:23:09 +0800

    T13/T13SideBar.cshtml Form

--- app/Controllers/ShoppingController.cs
+++ app/Controllers/ShoppingController.cs
@@ -24,7 +24,7 @@ namespace Landmark.Controllers
         public ActionResult GetJson(string childcategory)
         {
             Item shoppingCategory = Sitecore.Context.Database.GetItem(childcategory);
-            return Redirect(shoppingCategory.Paths.FullPath);
+            return Redirect(shoppingCategory.Children.First().Paths.FullPath);
             //return Content(Sitecore.Links.LinkManager.GetItemUrl(shoppingCategory));
         }
 
