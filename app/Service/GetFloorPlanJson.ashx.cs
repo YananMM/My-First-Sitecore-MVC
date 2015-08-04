0d01ca3056b3fbf97c209a235e9e7a739fd97930
YananMM <yananfun@outlook.com>
2015-08-04 13:46:38 +0800

    add floor object

---
+++ app/Service/GetFloorPlanJson.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Landmark.Service
+{
+    /// <summary>
+    /// Summary description for GetFloorPlanJson
+    /// </summary>
+    public class GetFloorPlanJson : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
