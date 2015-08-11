94a21fdc8a3f71f88540853c9aa55b20dbf940df
LilyJiang <lily.jiang@gruden.com>
2015-08-11 13:37:28 +0800

    T29 Renderings

---
+++ app/Helper/ArtTourHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+
+namespace Landmark.Helper
+{
+    public class ArtTourHelper
+    {
+        public Item GetArtistByArt(Item art)
+        {
+            Sitecore.Data.Fields.GroupedDroplinkField artistField = art.Fields["Artist"];
+            if (artistField != null)
+            {
+                return artistField.TargetItem;
+            }
+            return null;
+        }
+    }
+}
