12a2376a3b164391844fae1dc431a18680c1110d
LilyJiang <lily.jiang@gruden.com>
2015-08-13 16:52:03 +0800

    update html

--- app/Service/GetArtPieceJsonByArtists.ashx.cs
+++ app/Service/GetArtPieceJsonByArtists.ashx.cs
@@ -1,4 +1,17 @@
-﻿using Landmark.Classes;
+﻿// ***********************************************************************
+// Assembly         : Landmark
+// Author           : Jiang Lili
+// Created          : 08-13-2015
+//
+// Last Modified By : Jiang Lili
+// Last Modified On : 08-13-2015
+// ***********************************************************************
+// <copyright file="GetArtPieceJsonByArtists.ashx.cs" company="Gruden">
+//     Copyright (c) Gruden. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using Landmark.Classes;
 using Landmark.Helper;
 using Landmark.Models;
 using Sitecore.Data.Items;
@@ -8,6 +21,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Script.Serialization;
 
+// <summary>
+// The Service namespace.
+// </summary>
 namespace Landmark.Service
 {
     /// <summary>
@@ -15,12 +31,22 @@ namespace Landmark.Service
     /// </summary>
     public class GetArtPieceJsonByArtists : IHttpHandler
     {
+        /// <summary>
+        /// The _helper
+        /// </summary>
         private ArtTourHelper _helper;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetArtPieceJsonByArtists"/> class.
+        /// </summary>
         public GetArtPieceJsonByArtists()
         {
             _helper = new ArtTourHelper();
         }
+        /// <summary>
+        /// Enables processing of HTTP Web requests by a custom HttpHandler that implements the <see cref="T:System.Web.IHttpHandler" /> interface.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.Web.HttpContext" /> object that provides references to the intrinsic server objects (for example, Request, Response, Session, and Server) used to service HTTP requests.</param>
         public void ProcessRequest(HttpContext context)
         {
             try
@@ -40,6 +66,11 @@ namespace Landmark.Service
             }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether another request can use the <see cref="T:System.Web.IHttpHandler" /> instance.
+        /// </summary>
+        /// <value><c>true</c> if this instance is reusable; otherwise, <c>false</c>.</value>
+        /// <returns>true if the <see cref="T:System.Web.IHttpHandler" /> instance is reusable; otherwise, false.</returns>
         public bool IsReusable
         {
             get
