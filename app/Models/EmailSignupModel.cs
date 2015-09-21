b5667eed78ca600f27cc5232cf7b2387bcc62044
LilyJiang <lily.jiang@gruden.com>
2015-09-21 09:47:21 +0800

    T31

---
+++ app/Models/EmailSignupModel.cs
@@ -0,0 +1,110 @@
+﻿// ***********************************************************************
+// Assembly         : Landmark
+// Author           : Jiang Lili
+// Created          : 09-18-2015
+//
+// Last Modified By : Jiang Lili
+// Last Modified On : 09-18-2015
+// ***********************************************************************
+// <copyright file="EmailSignupModel.cs" company="Gruden">
+//     Copyright (c) Gruden. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Landmark.Models
+{
+    /// <summary>
+    /// Class EmailSignupModel.
+    /// </summary>
+    public class EmailSignupModel
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier.
+        /// </summary>
+        /// <value>The unique identifier.</value>
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>The title.</value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>The first name.</value>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>The email.</value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the channel.
+        /// </summary>
+        /// <value>The channel.</value>
+        public string Channel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the room.
+        /// </summary>
+        /// <value>The room.</value>
+        public string Room { get; set; }
+
+        /// <summary>
+        /// Gets or sets the building.
+        /// </summary>
+        /// <value>The building.</value>
+        public string Building { get; set; }
+
+        /// <summary>
+        /// Gets or sets the street.
+        /// </summary>
+        /// <value>The street.</value>
+        public string Street { get; set; }
+
+        /// <summary>
+        /// Gets or sets the area.
+        /// </summary>
+        /// <value>The area.</value>
+        public string Area { get; set; }
+
+        /// <summary>
+        /// Gets or sets the district.
+        /// </summary>
+        /// <value>The district.</value>
+        public string District { get; set; }
+
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>The city.</value>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        /// <value>The state.</value>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Gets or sets the postcode.
+        /// </summary>
+        /// <value>The postcode.</value>
+        public string Postcode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country.
+        /// </summary>
+        /// <value>The country.</value>
+        public string Country { get; set; }
+    }
+}
