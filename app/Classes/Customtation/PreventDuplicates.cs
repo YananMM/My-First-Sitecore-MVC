64961044a09fb802d1a2c42ea86be0b9d7704b9b
yanan <yananfun@gmail.com>
2015-10-13 15:15:47 +0800

    198

---
+++ app/Classes/Customtation/PreventDuplicates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Events;
+using Sitecore.Data.Items;
+using Sitecore.Events;
+using Sitecore.Mvc.Extensions;
+using Sitecore.SecurityModel;
+
+namespace Landmark.Classes.Customtation
+{
+    public class PreventDuplicates
+    {
+        public void OnItemCopying(object sender, EventArgs args)
+        {
+            using (new SecurityDisabler())
+            {
+                if(args == null)
+                    return;
+                else
+                {
+                    Item source = Event.ExtractParameter(args, 0) as Item;
+                    Item parent = Event.ExtractParameter(args, 1) as Item;
+                    string name = Event.ExtractParameter(args, 2) as string;
+                    if(parent !=null && !name.IsEmptyOrNull())
+                    foreach (Item currentItem in parent.GetChildren())
+                    {
+                        if ((name.ToLower() == currentItem.DisplayName.ToLower()))
+                        {
+                            ((SitecoreEventArgs)args).Result.Cancel = true;
+                            Sitecore.Context.ClientPage.ClientResponse.Alert
+                              ("Name " + currentItem.Name + " is already in use.Please use another name for the page.");
+                            return;
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
