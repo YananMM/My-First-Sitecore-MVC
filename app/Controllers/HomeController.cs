31965bb2f80b35953a59e3fb8bae99a9729a4f51
LilyJiang <lily.jiang@gruden.com>
2015-12-21 11:56:11 +0800

    EMIAL SIGNUP

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -216,7 +216,7 @@ namespace Landmark.Controllers
                         LastName = model.LastName,
                         Email = model.Email,
                         Channel = (model.Channel == "0") ? emailOnlyItem.Fields["Glossary Value"].ToString() : emailAndPostal.Fields["Glossary Value"].ToString(),
-                        Interest = model.Interests.Replace("," + model.Others, ""),
+                        Interest = !string.IsNullOrEmpty(model.Others) ? model.Interests.Replace("," + model.Others, "") : model.Interests,
                         Other_Interest = model.Others,
                         Room = model.Room,
                         Building = model.Building,
